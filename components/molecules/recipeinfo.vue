6b843694d8ed8d3b0448e0456c8b7f852f0d054c
Robbie verdurme <boeferrob@live.be>
2020-03-24 18:36:15 +0100

    [Add] infopage

---
+++ components/molecules/recipeinfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <md-list>
+    <md-list-item>
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <h1> {{ menu.title }} </h1>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-content> {{ menu.content }} </md-card-content>
+      </md-card>
+    </md-list-item>
+  </md-list>
+</template>
+
+<script>
+export default {
+  props: {
+    menu: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
