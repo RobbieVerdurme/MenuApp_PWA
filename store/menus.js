e710bc166c49c0a2b0ba015efdd5587b52b89b60
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-25 16:27:25 +0100

    [Add] backendcall in vuex

--- store/menus.js
+++ store/menus.js
@@ -35,3 +35,46 @@ export const getters = {
     return state.menus.filter(m => m.title.includes(filtertext) || m.ingredients.find(i => i.title.includes(filtertext)))
   }
 }
+
+/**
+ * mutations
+ */
+export const mutations = {
+  /**
+   * set menus with payload
+   * @param {*} state
+   * @param {Array} payload
+   */
+  setMenus (state, payload) {
+    state.menus = payload
+  }
+}
+
+/**
+ * actions
+ */
+export const actions = {
+  /**
+   * get menus from firebase
+   * @param {*} param0
+   */
+  async getMenusFromFirebase ({ commit }) {
+    const messageRef = this.$fireDb.ref('FoodList')
+
+    // get data
+    let snap
+    // eslint-disable-next-line no-useless-catch
+    try {
+      snap = await messageRef.once('value')
+    } catch (e) {
+      throw e
+    }
+
+    // convert json to array of objects
+    const res = Object.keys(snap.val().Food).map(function (key) {
+      return snap.val().Food[key]
+    })
+
+    commit('setMenus', res)
+  }
+}
