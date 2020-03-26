54b450a31a421fe56c2da8c98b33e8535b217a44
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-26 16:07:19 +0100

    [Update] profilepage

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,16 +1,48 @@
 <template>
-  <button @click="logout">
-    Logout
-  </button>
+  <md-list>
+    <md-list-item>
+      <md-card>
+        <md-card-header>
+          <div class="md-title">
+            Profile {{ user.displayName }}
+          </div>
+          <md-card-header>
+            <md-card-content>
+              <table>
+                <tr>
+                  <td>Email : </td>
+                  <td>{{ user.email }}</td>
+                </tr>
+              </table>
+            </md-card-content>
+            <md-progress-bar v-if="sending" md-mode="indeterminate" />
+            <md-card-actions>
+              <md-button class="md-primary" :disabled="sending" @click="logout">
+                Logout
+              </md-button>
+            </md-card-actions>
+          </md-card-header>
+        </md-card-header>
+      </md-card>
+    </md-list-item>
+  </md-list>
 </template>
 
 <script>
 export default {
   middleware: 'authenticated',
+  data () {
+    return {
+      user: this.$fireAuth.currentUser,
+      sending: false
+    }
+  },
   methods: {
     logout () {
+      this.sending = true
       this.$store.dispatch('user/signOut')
         .then(() => {
+          this.sending = false
           this.$router.push({ name: 'login' })
         }).catch((err) => {
           alert(err)
