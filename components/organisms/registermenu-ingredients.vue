222b41c1e8ca7de1496ee3392326f2c0bc4d1d75
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-06 20:34:02 +0200

    [Add] add ingredient to menu

---
+++ components/organisms/registermenu-ingredients.vue
@@ -0,0 +1,51 @@
+<template>
+  <md-list>
+    <!--list of ingredients-->
+    <md-list-item v-for="ingredient in menu.ingredients" :key="ingredient.id">
+      <addIngredient :edit="true" :ingredient="ingredient" @editIngredient="editIngredient" @deleteIngredient="deleteIngredient" />
+    </md-list-item>
+    <!--/list of ingredients-->
+
+    <!--Add new ingredient item-->
+    <md-list-item>
+      <addIngredient :edit="false" @addIngredient="addIngredient" />
+    </md-list-item>
+    <!--/Add new ingredient item-->
+  </md-list>
+</template>
+
+<script>
+export default {
+  components: {
+    addIngredient: () => import('~/components/molecules/registermenu-ingredient')
+  },
+  props: {
+    menu: Object
+  },
+  methods: {
+    /**
+     * add ingredient to list of menu
+     */
+    addIngredient (value) {
+      this.menu.ingredients.push(value)
+    },
+
+    /**
+    * edit ingredient of list
+    */
+    editIngredient (value) {
+      const index = this.menu.ingredients.indexOf(value.oldIngredient)
+      this.menu.ingredients[index] = value.newIngredient
+      alert('Edit ingredient ' + value.newIngredient.name)
+    },
+
+    /**
+     * delete ingredient form list
+     */
+    deleteIngredient (value) {
+      const index = this.menu.ingredients.indexOf(value)
+      this.menu.ingredients.splice(index, 1)
+    }
+  }
+}
+</script>
---
+++ pages/menu/register/ingredients/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <editIngredients :menu="menu" />
+</template>
+
+<script>
+export default {
+  components: {
+    editIngredients: () => import('~/components/organisms/registermenu-ingredients')
+  },
+  props: {
+    menu: Object
+  }
+
+}
+</script>
