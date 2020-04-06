c7425edea2b41b6ed7ad249ab996b9b27e571680
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-06 20:34:32 +0200

    [Add] add preperation to menu page

---
+++ pages/menu/register/make/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <editPreperation :menu="menu" @setMenuPreperation="setMenuPreperation" />
+</template>
+
+<script>
+export default {
+  components: {
+    editPreperation: () => import('~/components/molecules/registermenu-preperation')
+  },
+  props: {
+    menu: Object
+  },
+  methods: {
+    /**
+     * set menu perperation
+     */
+    setMenuPreperation (value) {
+      this.menu.preperation = value
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
