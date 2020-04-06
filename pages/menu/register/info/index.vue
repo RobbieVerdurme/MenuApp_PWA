c520784471eac308e20122edbc4b138eb8893abd
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-06 20:34:50 +0200

    [Add] addinfo to menu page

---
+++ pages/menu/register/info/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <editInfo :menu="menu" @setMenuTitle="setMenuTitle" @setMenuDescription="setMenuDescription" />
+</template>
+
+<script>
+export default {
+  components: {
+    editInfo: () => import('~/components/molecules/registermenu-info')
+  },
+  props: {
+    menu: Object
+  },
+  methods: {
+    /**
+     * set menu title
+     */
+    setMenuTitle (value) {
+      this.menu.name = value
+    },
+
+    /**
+     * set menu desciption
+     */
+    setMenuDescription (value) {
+      this.menu.discritpion = value
+    }
+  }
+}
+</script>
