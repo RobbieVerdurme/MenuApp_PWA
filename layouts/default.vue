5fa4849b0c467f95f8f8de8f7555daa49cf1df88
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-17 17:40:45 +0200

    [Update] code to static/images

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 <style scoped>
 .md-app-content {
-  background-image: linear-gradient(to bottom, rgb(255, 255, 255, 0.9) 0%,rgba(255, 255, 255,0.9) 100%), url('/Background_Ingedients.jpg');
+  background-image: linear-gradient(to bottom, rgb(255, 255, 255, 0.9) 0%,rgba(255, 255, 255,0.9) 100%), url('/images/Background_Ingedients.jpg');
   background-repeat: no-repeat;
   background-size: cover;
   height: 90vh;
